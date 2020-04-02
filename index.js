cb21b0a32b2ff875ca7a67600f574704efaac559
Marco <marcoasjunior90@gmail.com>
2020-04-02 19:26:16 -0300

    try

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const Strategy = require('./utils/passport')
 
 app.use(cors())
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // Auth
 
